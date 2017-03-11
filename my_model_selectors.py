46980c73d6f2898b08c4446b742aef262c70f181
Anthony Daniell <anthonydaniell@anthonys-mbp.attlocal.net>
2017-03-11 03:54:47 -0800

    Sync to latest master and added my code.

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -76,7 +76,8 @@ class SelectorBIC(ModelSelector):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        return None
+#        raise NotImplementedError
 
 
 class SelectorDIC(ModelSelector):
@@ -92,7 +93,8 @@ class SelectorDIC(ModelSelector):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        return None
+#        raise NotImplementedError
 
 
 class SelectorCV(ModelSelector):
@@ -104,4 +106,110 @@ class SelectorCV(ModelSelector):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection using CV
-        raise NotImplementedError
+        
+        #
+        # Need to loop over range of possible numbers of hidden states (outer loop)
+        # and cross-fold validations, computing the average likelihood for each number of hidden
+        # states.  Return the HMM with the greatest average likelihood.
+        #
+        # model = SelectorCV(sequences, Xlengths, word, 
+        #            min_n_components=2, max_n_components=15, random_state = 14).select()
+        #
+        print()
+        print("-----")
+        print("SelectorCV: self.this_word: ", self.this_word)
+        print("SelectorCV: X:")
+        print(self.X)
+        print("SelectorCV: self.lengths:")
+        print(self.lengths)
+        print("SelectorCV: length of self.lengths = ", len(self.lengths))
+        print("SelectorCV: self.min_n_components = ", self.min_n_components)
+        print("SelectorCV: self.max_n_components = ", self.max_n_components)
+        print()
+        
+        # The number of splits cannot be greater than number of samples and also must be >=2 (per KFold doc)
+        # If len(self.length) is < 3, then let 2 be selected automatically, otherwise set to 3 splits.
+        minSplit = min(len(self.lengths),3)
+        print("SelectorCV: minSplit = ", minSplit)
+        split_method = KFold(n_splits=minSplit)
+        
+        #initialize max log likelihood over search space
+        maxLL = float('-inf')
+        # Initialize the bestModel to return
+        bestModel = None
+        best_num_components=3 #default size in case we don't find better model (due to errors etc.)
+                    
+        for iHidden in range(self.min_n_components, self.max_n_components+1):
+            print("SelectorCV: iHidden = ", iHidden)
+            # initialize the avg log likelihood for the current number of hidden nodes
+            avgLL = 0.0
+            for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+###                print("SelectorCV: Train fold indices:{} Test fold indices:{}".format(cv_train_idx, cv_test_idx))  # view indices of the folds
+        
+                # Use concatenation formalism from asl_data.py for hmm data format
+                X_train_cat = []
+                lengths_train = []
+                
+                # Create new arrays that extract the k-fold split indices for train and test hold-outs
+                for iExtract in cv_train_idx:
+###                    print("SelectorCV: iExtract (train) = ", iExtract)
+                    X_train_cat = X_train_cat+self.sequences[iExtract]
+                    lengths_train.append(self.lengths[iExtract])
+
+                X_train = np.array(X_train_cat)
+###                print("SelectorCV: X_train:")
+###                print(X_train)
+###                print("SelectorCV: lengths_train:")
+###                print(lengths_train)
+                
+                # Now do similar setup for the holdout(test)/ cross-validation sequences.
+                
+                X_holdout_cat = []
+                lengths_holdout = []
+                
+                for iExtract in cv_test_idx:
+###                    print("SelectorCV: iExtract (test) = ", iExtract)
+                    X_holdout_cat = X_holdout_cat+self.sequences[iExtract]
+                    lengths_holdout.append(self.lengths[iExtract])
+
+                X_holdout = np.array(X_holdout_cat)
+###                print("SelectorCV: X_holdout:")
+###                print(X_holdout)
+###                print("SelectorCV: lengths_holdout:")
+###                print(lengths_holdout)
+                  
+                # Train on extracted  subset of sequences for k-fold
+                model = GaussianHMM(n_components=iHidden, n_iter=1000).fit(X_train, lengths_train)
+              
+                # Want to score on the hold-out samples
+                logL = model.score(X_holdout, lengths_holdout)
+                print("SelectorCV: (holdout) logL = {}".format(logL))
+                avgLL = avgLL + logL
+
+            # Compute average log likelihood over the number of k-fold splits. (either 2 or 3)
+            avgLL = avgLL/(1.0*minSplit)
+            print("SelectorCV: avgLL = ", avgLL)
+            
+            # Save this model parameters if it has the highest avgLL so far
+            if avgLL > maxLL:
+                print("SelectorCV: Updating best model.")
+                maxLL=avgLL
+                bestModel = model
+                best_num_components=iHidden
+                print ("SelectorCV: maxLL = ", maxLL)
+                print ("SelectorCV: best_num_components = ", best_num_components)
+                print ("SelectorCV: bestModel = ", model)
+            
+            print()
+        
+        if bestModel == None:  # Return a default case if there was a problem
+            return self.base_model(best_num_components)
+        else:
+            # This is the final model in the best group.
+            # Could have picked another one (best fit in that group)
+            # but this is probably adaquate, as long as it came from the 
+            # best group of fits
+            print("SelectorCV: Returning a best fit model.")
+            return bestModel
+
+
--- my_recognizer.py
+++ my_recognizer.py
@@ -1,5 +1,6 @@
 import warnings
 from asl_data import SinglesData
+import numpy as np
 
 
 def recognize(models: dict, test_set: SinglesData):
@@ -22,4 +23,39 @@ def recognize(models: dict, test_set: SinglesData):
     guesses = []
     # TODO implement the recognizer
     # return probabilities, guesses
-    raise NotImplementedError
+    
+    # Loop over each word and then run all the models on that word, retaining
+    # the log likelihood values for that word/model combination
+#    print("recognize 5.")
+    
+    # get the feature sequences and lengths for each test word
+    iTestWord = test_set.get_all_Xlengths()
+    
+    # initialize best guess word variable
+    bestGuessWord = None
+    # Loop over each word, and the each model to find max likelihood model-word.
+    for iWordIdx, iWordTuple in iTestWord.items():
+        maxLL = float('-inf')
+        testSequence = np.array(iWordTuple[0]) #hmm friendly format
+        testLen = np.array(iWordTuple[1]) # hmm friendly format
+#        print("recognize: testSequence = ", testSequence)
+#        print("recognize: testLen = ", testLen)
+        # Loop over each model and retain highest likelihood word
+        for iModelWord, iModel in models.items():
+            # Error trap for models that don't work for some reason.
+            try:
+                logL = iModel.score(testSequence, testLen)
+#                print("recognize: logL = {}".format(logL))
+            except:
+                logL = float('-inf')  # set to default value for a failed model.
+            # retain highest likelihood word
+            if logL > maxLL:
+                maxLL=logL
+                bestGuessWord = iModelWord
+#                print("recognize: bestGuessWord = ", bestGuessWord)
+
+        # update guesses list with highest likelihood word
+        guesses.append(bestGuessWord)
+        
+    return probabilities, guesses
+#    raise NotImplementedError
