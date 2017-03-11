a10d514fe2a529b3ee6775ed83261ce8ee24a06e
Anthony Daniell <anthonydaniell@anthonys-mbp.attlocal.net>
2017-03-11 10:00:32 -0800

    Cleanup of print statements in my_model_selectors.

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -112,46 +112,31 @@ class SelectorCV(ModelSelector):
         # and cross-fold validations, computing the average likelihood for each number of hidden
         # states.  Return the HMM with the greatest average likelihood.
         #
-        # model = SelectorCV(sequences, Xlengths, word, 
+        # Pattern for model call:  SelectorCV(sequences, Xlengths, word, 
         #            min_n_components=2, max_n_components=15, random_state = 14).select()
         #
         # Let's be aggressive on run-time warnings, such as those issued from the modeling section below.
         warnings.filterwarnings("error", category=RuntimeWarning) # Raise an exception.
-###        print()
-###        print("-----")
-###        print("SelectorCV: self.this_word: ", self.this_word)
-###        print("SelectorCV: X:")
-###        print(self.X)
-###        print("SelectorCV: self.lengths:")
-###        print(self.lengths)
-###        print("SelectorCV: length of self.lengths = ", len(self.lengths))
-###        print("SelectorCV: self.min_n_components = ", self.min_n_components)
-###        print("SelectorCV: self.max_n_components = ", self.max_n_components)
-###        print()
         
         # The number of splits cannot be greater than number of samples and also must be >=2 (per KFold doc)
         # If len(self.length) is < 3, then let 2 be selected automatically, otherwise set to 3 splits.
         if len(self.lengths) < 2:
-###            print("SelectorCV: Returning fallback model.  Need at least two word samples to train/test model.")
             return None  # self.base_model(self.n_constant)
         minSplit = min(len(self.lengths),3)
-###        print("SelectorCV: minSplit = ", minSplit)
         split_method = KFold(n_splits=minSplit)
         
-        #initialize max log likelihood over search space
+        #initialize global max log likelihood over search space
         maxLL = float('-inf')
         # Initialize the bestModel to return
         bestModel = None
                     
         for iHidden in range(self.min_n_components, self.max_n_components+1):
-###            print("SelectorCV: iHidden = ", iHidden)
             # initialize the avg log likelihood for the current number of hidden nodes
             avgLL = 0.0 # this is an accumulator, so okay to set to zero
             validSplit = 0 # Keep track of number of splits that can be trained & scored.
             maxLocalLL = float('-inf') # best log likelihood for this number of hidden nodes
             bestLocalModel = None  # store the best model of the group for this number of hidden nodes.
             for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
-###                print("SelectorCV: Train fold indices:{} Test fold indices:{}".format(cv_train_idx, cv_test_idx))  # view indices of the folds
         
                 # Use concatenation formalism from asl_data.py for hmm data format
                 X_train_cat = []
@@ -159,30 +144,20 @@ class SelectorCV(ModelSelector):
                 
                 # Create new arrays that extract the k-fold split indices for train and test hold-outs
                 for iExtract in cv_train_idx:
-###                    print("SelectorCV: iExtract (train) = ", iExtract)
                     X_train_cat = X_train_cat+self.sequences[iExtract]
                     lengths_train.append(self.lengths[iExtract])
 
                 X_train = np.array(X_train_cat)
-###                print("SelectorCV: X_train:")
-###                print(X_train)
-###                print("SelectorCV: lengths_train:")
-###                print(lengths_train)
                 
                 # Now do similar setup for the holdout(test)/ cross-validation sequences.               
                 X_holdout_cat = []
                 lengths_holdout = []
                 
                 for iExtract in cv_test_idx:
-###                    print("SelectorCV: iExtract (test) = ", iExtract)
                     X_holdout_cat = X_holdout_cat+self.sequences[iExtract]
                     lengths_holdout.append(self.lengths[iExtract])
 
                 X_holdout = np.array(X_holdout_cat)
-###                print("SelectorCV: X_holdout:")
-###                print(X_holdout)
-###                print("SelectorCV: lengths_holdout:")
-###                print(lengths_holdout)
                   
                 # Train on extracted  subset of sequences for k-fold
                 # Error trap for bad training or scoring cases.
@@ -192,48 +167,34 @@ class SelectorCV(ModelSelector):
                     # Want to score on the hold-out samples
                     logL = model.score(X_holdout, lengths_holdout)
                     validSplit = validSplit+1 # Valid split found (trained & scored)
-###                    print("SelectorCV: Valid (holdout) logL = {}".format(logL))
                     # Keep track of the best model within this hidden node group.
                     if logL > maxLocalLL:
                         maxLocalLL = logL
                         bestLocalModel = model
-###                        print("SelectorCV: Best Log Likelihood - retaining model: ", maxLocalLL)
                 except: #If there are any issues with training or scoring, set log likelihood to zero
-###                    print("SelectorCV: Issue with models.  Setting LL accumulator to 0.")
                     logL = 0
-###                print("SelectorCV: (holdout) logL = {}".format(logL))
+                    
                 avgLL = avgLL + logL
 
             # Compute average log likelihood over the number of valid k-fold splits.
             if validSplit > 0:
-###                print("SelectorCV: Valid model found with splits = ", validSplit)
                 avgLL = avgLL/(1.0*validSplit)
             else:
-###                print("SelectorCV: Skipping model.")
                 avgLL = float('-inf') # skip this model entirely (won't pass next if statement for updating model)
-###            print("SelectorCV: avgLL = ", avgLL)
             
             # Save this model parameters if it has the highest avgLL so far
             if avgLL > maxLL:
-###                print("SelectorCV: Updating best model.")
                 maxLL=avgLL
                 bestModel = bestLocalModel  # choose the best model in group so far (arbitrarily)
-                best_num_components=iHidden
-###                print ("SelectorCV: maxLL = ", maxLL)
-###                print ("SelectorCV: best_num_components = ", best_num_components)
-###                print ("SelectorCV: bestModel = ", model)
-            
-###            print()
+
         
         if bestModel == None:  # Return a default case if there was a problem
-###            print("SelectorCV: Returning default fallback model - None.")
             return None
         else:
             # This is the final model in the best group.
             # Could have picked another one (best fit in that group)
             # but this is probably adaquate, as long as it came from the 
             # best group of fits
-###            print("SelectorCV: Returning a best fit model.")
             return bestModel
 
 
